9d49118432104c8da83749369456f264e526016e
Ali <ali@arsh.ir>
2017-10-06 13:01:58 +0330

    Signed-off-by: Ali <ali@arsh.ir>

--- dbtest.php
+++ dbtest.php
@@ -1,11 +1,26 @@
 <?php
+echo "start:";
+
+// procedural style
+$mysqli = mysqli_connect(
+    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
+    getenv('OPENSHIFT_MYSQL_DB_USERNAME'), 
+    getenv('OPENSHIFT_MYSQL_DB_HOST'), 
+    getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+    getenv('OPENSHIFT_MYSQL_DB_PORT')
+);
+
+var_dump($mysqli);
+
+/*
+
 $dbhost = getenv('OPENSHIFT_MYSQL_DB_HOST'); // Host name 
 $dbport = getenv('OPENSHIFT_MYSQL_DB_PORT'); // Host port
 $dbusername = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); // MySQL username 
 $dbpassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD'); // MySQL password 
 $db_name = getenv('OPENSHIFT_GEAR_NAME'); // Database name 
 
-echo "start:";
+
 echo $dbhost . ":" . $dbport;
 echo $db_name . " " . $dbusername;
 
@@ -14,7 +29,7 @@ $mysqlCon = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_
 mysqli_select_db($mysqlCon, getenv('OPENSHIFT_APP_NAME')) or die("Error: " . mysqli_error($mysqlCon));
 
 var_dump($mysqlCon);
-
+*/
 echo "end";
 
 ?>
