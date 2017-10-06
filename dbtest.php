7b4cedc14062c53e616b7a318368803277afb9de
Ali <ali@arsh.ir>
2017-10-06 12:51:24 +0330

    db

--- dbtest.php
+++ dbtest.php
@@ -5,12 +5,16 @@ $dbusername = getenv('OPENSHIFT_MYSQL_DB_USERNAME'); // MySQL username
 $dbpassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD'); // MySQL password 
 $db_name = getenv('OPENSHIFT_GEAR_NAME'); // Database name 
 
+echo "start:";
+echo $dbhost . ":" . $dbport;
 echo $db_name . " " . $dbusername;
 
 $mysqlCon = mysqli_connect(getenv('OPENSHIFT_MYSQL_DB_HOST'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'), "", getenv('OPENSHIFT_MYSQL_DB_PORT')) or die("Error: " . mysqli_error($mysqlCon));
 
 mysqli_select_db($mysqlCon, getenv('OPENSHIFT_APP_NAME')) or die("Error: " . mysqli_error($mysqlCon));
 
+var_dump($mysqlCon);
 
+echo "end";
 
 ?>
